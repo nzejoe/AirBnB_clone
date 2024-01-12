92f0bbfcac567dd08903c4f3df761ac2e1ded0c4
nzejoe <jonathannzete@gmail.com>
2024-01-12 12:19:50 +0100

    1. Be pycodestyle compliant!

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a function that greet user."""
+
+def greet(name: str) -> None:
+    """
+    Print a greeting message.
+
+    Args:
+        name (str): The name to greet.
+    """
+    print(f"Hello, {name}!")
